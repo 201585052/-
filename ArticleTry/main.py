91697491768ba8d6ae0d4e3517f908ed3aa10085
201585052 <1216884492@qq.com>
2018-10-31 20:23:54 +0800

    bm

--- ArticleTry/main.py
+++ ArticleTry/main.py
@@ -5,5 +5,6 @@ import os
 
 # print (os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(['scrapy','crawl','jobbole'])
+# execute(['scrapy','crawl','jobbole'])
+execute(['scrapy','crawl','zhihu'])
 # execute(['scrapy','shell','http://blog.jobbole.com/114413/'])
